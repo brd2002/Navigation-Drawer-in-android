93bb8bf62c483efbc4a52de6def36a513836f34b
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-28 15:40:50 +0530

    completed navigation drawer

--- app/src/main/java/com/example/navigationdrawerapp/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawerapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.navigationdrawerapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.navigationdrawerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // to open navigation drawer on click menu button
+        binding.toolBar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+        }
+        // set on click listener on menu items
+        binding.navView.setNavigationItemSelectedListener {
+            binding.drawerLayout.close()
+            it.isChecked = true
+            when(it.itemId){
+                R.id.profile -> {
+                 startActivity(Intent(this , ProfileActivity::class.java))
+                }
+                R.id.check_heart -> {
+                    startActivity(Intent(this, CheckHeartActivity::class.java))
+                }
+                R.id.medical_report -> {
+
+                }
+                R.id.share -> {
+                }
+                R.id.wallet -> {
+
+                }
+                R.id.calendar -> {
+
+                }
+                R.id.settings -> {
+                    startActivity(Intent(this  , SettingsActivity::class.java))
+                }
+                R.id.contact_us -> {
+
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
     }
 }
